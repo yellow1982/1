9426fb3576cd01168b07e9c29df6a3a847381428
Илья <Yellow82@inbox.ru>
2025-01-29 16:18:15 +0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,18 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+x = 2 * 2 + 2
+y = 2 * (2 + 2)
+print(x)
+print(y)
+print(x == y)
+
+# 4th program
+x = float("123.456")
+x = int(x * 10)
+print (x % 1000 % 100 % 10)
